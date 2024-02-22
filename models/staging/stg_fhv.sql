d5770bbba7290559be1e0fa009120b918781759a
Michał Kępczyński <michal.kepczynski@gmail.com>
2024-02-22 08:44:16 +0000

    20240222

--- models/staging/stg_fhv.sql
+++ models/staging/stg_fhv.sql
@@ -1,12 +1,17 @@
-{{ config(materialized='view') }}
+{{ config(materialized="view") }}
 
 select 
-    dispatching_base_num
-    , cast(pickup_datetime as timestamp) as pickup_datetime
-    , cast(dropOff_datetime as timestamp) as dropoff_fatetime 
-    , cast(PUlocationID as numeric) as pickup_location_id
-    , cast(DOlocationID as numeric) as dropoff_location_id
-    , cast(SR_Flag as numeric) as sr_flag
-    , Affiliated_base_number as affiliated_base_number
-from {{ source('staging','fhv_tripdata_ext') }}
-limit 10
+    dispatching_base_num,
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropOff_datetime as timestamp) as dropoff_datetime,
+    cast(pulocationid as integer) as pickup_locationid,
+    cast(dolocationid as integer) as dropoff_locationid,
+    cast(sr_flag as integer) as is_shared_ride,
+    affiliated_base_number
+from {{ source("staging", "fhv_trip_ext") }}
+
+{% if var('is_test_run', default=true) %}
+
+    limit 100
+
+{% endif %}
